c9689669ededa58e19f8e087c4f32bf59502f370
anymore131 <1628512812@qq.com>
2024-06-24 00:04:08 +0800

    1

---
+++ src/main/java/cn/edu/zust/se/POJO/User.java
@@ -0,0 +1,35 @@
+package cn.edu.zust.se.POJO;
+
+import lombok.Data;
+
+/**
+ * @author Lenovo
+ */
+@Data
+public class User {
+    int id;
+    //真实姓名
+    String name;
+    //用户名
+    String userName;
+    //密码
+    String password;
+    int age;
+    //性别
+    String gender;
+    String introduction;
+    //权限
+    int type;
+    String email;
+    String mobile;
+    //省
+    String province;
+    //城市
+    String city;
+    //具体地址
+    String address;
+    String QQ;
+    String weixin;
+    //在线状态
+    int status;
+}
---
+++ src/main/java/cn/edu/zust/se/Vo/UserVo.java
@@ -0,0 +1,33 @@
+package cn.edu.zust.se.Vo;
+
+import lombok.Data;
+
+/**
+ * @author Lenovo
+ */
+@Data
+public class UserVo {
+    int id;
+    //真实姓名
+    String name;
+    //用户名
+    String userName;
+    int age;
+    //性别
+    String gender;
+    String introduction;
+    //权限
+    int type;
+    String email;
+    String mobile;
+    //省
+    String province;
+    //城市
+    String city;
+    //具体地址
+    String address;
+    String QQ;
+    String weixin;
+    //在线状态
+    int status;
+}
